39a265fc46f4941f6a5817954e0c61c885547336
Matthew McGee <mlmcgeenc@gmail.com>
2025-01-07 09:17:53 -0500

    update input to use enter key to add word

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,6 +5,13 @@ import { InputType } from '../types/types';
 
 const Input = ({ setInputValue, handleAddWord }: InputType) => {
 	const { inputValue } = useContext(AppContext);
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+			if (e.key === 'Enter') {
+				handleAddWord(inputValue);
+			}
+		};
+
 	return (
 		<div className='bg-green-950 max-h-14 flex justify-between rounded m-4 border border-green-800 placeholder-green-800 text-green-600 text-base p-2 focus-within:bg-green-600'>
 			<input
@@ -12,6 +19,7 @@ const Input = ({ setInputValue, handleAddWord }: InputType) => {
 				id='input'
 				value={inputValue}
 				onChange={(e) => setInputValue(e.target.value)}
+				onKeyDown={handleKeyDown}
 				className='peer bg-transparent grow placeholder-green-800 focus:placeholder-green-950 text-green-600 focus:text-black text-base p-2 focus:border-green-600 focus:outline-none focus:ring-0'
 				placeholder='Type word here'
 			/>
